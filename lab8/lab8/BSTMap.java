fb9b317abf2eaa20ab0d1a55b16a70227cc6d363
ZhanLiangUF <zliang@ufl.edu>
2016-10-08 17:39:33 -0400

    add get function for BST

--- lab8/lab8/BSTMap.java
+++ lab8/lab8/BSTMap.java
@@ -27,14 +27,30 @@ public class BSTMap<K extends Comparable<Key>, Value> implements Map61B<K, V> {
 
  }
 
+ public Value get(Key key) {
+   return get(root,key);
+ }
+ 
+ private Value get(Node x,Key key) {
+   if (x == null) return null;
+   int cmp = key.compareTo(x.key);
+   if (cmp < 0) {
+     get(x.left, key);
+   } else if (cmp > 0) {
+     get(x.right, key);
+   } else {
+     return x.val;
+   }
+ }
+
  private Node putHelper(Node x, Key key, Value val) {
    if (x == null) {
      return new Node(key, val, 1);
    }
    int cmp = key.compareTo(x.key);
-   if (cmp < 0) { // cmp is negative if this.key < input key
+   if (cmp < 0) { // cmp is negative if this.key > input key
      x.left  = putHelper(x.left, key, val);
-   } else if (cmp > 0) {// cmp is positive if this.key > input key
+   } else if (cmp > 0) {// cmp is positive if this.key < input key
      x.right = putHelper(x.right, key, val);
    } else {
      x.val = val;
@@ -43,16 +59,20 @@ public class BSTMap<K extends Comparable<Key>, Value> implements Map61B<K, V> {
    return x;
  }
 
- public int size() {
-   if (root == null) {
-     return 0;
-   }
-   return root.size;
- }
+
+public int size() {
+  return size(root);
+}
+
+private int size(Node x) {
+  if (x == null) return 0;
+  else return x.size;
+}
 
  public void clear() {
    root == null
  }
+
 
 
 
